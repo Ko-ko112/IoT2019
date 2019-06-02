78ad4bb044ce28878920806497d64ac8be364766
Your Name <ruengrid.wi@mail.wu.ac.th>
2019-06-03 01:47:23 +0700

    1

--- note.js
+++ note.js
@@ -1,14 +1,17 @@
+//git add ___    , git commit -m 1 
+//app.get(()=>{})
+
 const express = require('express')
 var app = express()
 
-//app.get(()=>{})
+
 app.get('/:input', (req,res) => {
     res.send(req,params.inout)
 })
 
 
 app.get('/webhook', (req, res) => {
-    // push block
+    // push blockgit
     let msg = 'Hello World!'
     push(msg)
     res.send(msg)
